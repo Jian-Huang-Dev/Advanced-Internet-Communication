30e3231ccc5a1b59d04682ca9f5782869e906c1c
Jian Huang <johnbodun@hotmail.com>
2015-02-13 08:33:44 -0500

    4DN4 assignment 1 part A

--- assignment_1/Dijkstra_shortest_path.m
+++ assignment_1/Dijkstra_shortest_path.m
@@ -1,16 +1,72 @@
-function [ D, P ] = Dijkstra_shortest_path( src, W )
+%--------------------------------------------------------------------------
+% BASIC DIJKSTRA'S SHORTEST PATH
+%--------------------------------------------------------------------------
+% return vector D,where D(1,j) == shortest distance between (src,j)
+% return vecotr P,where P(j) is predecessor node on the shortest
+%path from node j to the source
+%--------------------------------------------------------------------------
 
-% get the size of weight matrix
-[N, N] = size(W);
 
-D = ones(1, N) * inf;
-P = ones(1, N) * inf;
+function [D,P] = Dijkstra_shortest_path(src, T, W, N)
 
-% unvisited nodes
-U = ones(1, N);
-% src node is visited initiallly
-U(src) = 0;
-D(1, src) = 0;
+D = inf * ones(1,N);
+
+P = inf * ones(1,N);
+
+UV = ones(1,N); %all node labelled unvisited initially
+
+UV(src) = 0; 
+
+D(1,src) = 0;
+
+pass = 1;
+
+v = src;
+
+while pass < 16
+    
+    fprintf('this is the %g round\n',pass);
+    
+    
+    
+    for i = 1:N 
+          
+        
+        if UV(i) == 1 && T(v,i) == 1
+            if D(v)+ W(v,i) < D(i)
+                D(i) = D(v)+ W(v,i);
+                P(i) = v;
+            end
+        end
+        
+    end
+    
+
+    UV(v) = 0;
+    pass = pass + 1; 
+    v = Find_Closest_Node(UV,D,N);
+    D
+    P
+
+    
+end
+
+end
+
+function next_node = Find_Closest_Node(UV,D,N)
+
+min_dis = inf;
+next_node = 0;
+
+for u = 1:N
+    if UV(u) == 1 
+        if D(u) < min_dis
+            min_dis = D(u);
+            next_node = u;
+        end
+        
+    end
+end
 
 end
 
